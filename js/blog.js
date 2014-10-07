f65ecbccb8ed4bf25546af1615c7307ed520ec9f
levi <arclevi@outlook.com>
2014-10-07 10:31:57 +0800

    Working with Data

--- js/blog.js
+++ js/blog.js
@@ -5,10 +5,27 @@ $(function() {
     // Replace this line with the one on your Quickstart Guide Page
     Parse.initialize("mXkBQqvnQRuuLaWn90JhvPKZHuBnopDVNpApRNrW", "fgAnjH6VPKQHfGNyiI1e2w5GmfrUP10mw26TQyVE");
  
-    var TestObject = Parse.Object.extend("TestObject");
-    var testObject = new TestObject();
-    testObject.save({foo: "bar"}).then(function(object) {
-      alert("yay! it worked");
+    var Blog = Parse.Object.extend("Blog");
+    var Blogs = Parse.Collection.extend({
+        model: Blog
+    });
+    var BlogsView =  Parse.View.extend({
+        template: Handlebars.compile($('#blogs-tpl').html()),
+        render: function(){ 
+            var collection = { blog: this.collection.toJSON() };
+            this.$el.html(this.template(collection));
+        }
+    });
+    var blogs = new Blogs();
+    blogs.fetch({
+        success: function(blogs) {
+            var blogsView = new BlogsView({ collection: blogs });
+            blogsView.render();
+            $('.main-container').html(blogsView.el);
+        },
+        error: function(blogs, error) {
+            console.log(error);
+        }
     });
  
 });
